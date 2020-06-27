cc209ff16c2668d827b3cafbea592c029fb124b0
Felipe Jacson de Freitas <felipejacson@hotmail.com>
2020-06-27 15:29:02 -0300

    Initial commit

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into procedimento_execucao(procedimento_id, sexo, idade, autoriza) values(1234, 0, 10, 1);
-insert into procedimento_execucao(procedimento_id, sexo, idade, autoriza) values(4567, 0, 20, 1);
-insert into procedimento_execucao(procedimento_id, sexo, idade, autoriza) values(6789, 1, 10, 1);
-insert into procedimento_execucao(procedimento_id, sexo, idade, autoriza) values(6789, 0, 10, 1);
-insert into procedimento_execucao(procedimento_id, sexo, idade, autoriza) values(1234, 0, 20, 1);
-insert into procedimento_execucao(procedimento_id, sexo, idade, autoriza) values(4567, 1, 30, 1);
